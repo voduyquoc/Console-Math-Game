3cec1569ad463ef064828686cd3b97d9c0decb38
voduyquoc <voduyquockt@gmail.com>
2023-09-13 16:12:47 +0200

    Add project files.

---
+++ FirstProgram/Helpers.cs
@@ -0,0 +1,76 @@
+﻿using FirstProgram.Models;
+
+namespace FirstProgram
+{
+    internal class Helpers
+    {
+        static List<Game> games = new();
+
+        internal static void PrintGames()
+        {
+            Console.Clear();
+            Console.WriteLine("Game History:");
+            Console.WriteLine("----------------------");
+            foreach (var game in games)
+            {
+                Console.WriteLine($"{game.Date} - {game.Type}: {game.Score}pts.");
+            }
+            Console.WriteLine("----------------------\n");
+            Console.WriteLine("Press any key to go back to the main menu.");
+            Console.ReadLine();
+        }
+
+        internal static void AddToHistory(int gameScore, GameType gameType)
+        {
+            games.Add(new Game
+            {
+                Date = DateTime.Now,
+                Score = gameScore,
+                Type = gameType
+            });
+        }
+
+        internal static int[] GetDivisionNumber()
+        {
+            var random = new Random();
+            var firstNumber = random.Next(1, 99);
+            var secondNumber = random.Next(1, 99);
+
+            var result = new int[2];
+
+            while (firstNumber % secondNumber != 0)
+            {
+                firstNumber = random.Next(1, 99);
+                secondNumber = random.Next(1, 99);
+            }
+
+            result[0] = firstNumber;
+            result[1] = secondNumber;
+
+            return result;
+        }
+
+        internal static string? ValidateResult(string result)
+        {
+            while (string.IsNullOrEmpty(result) || !Int32.TryParse(result, out _))
+            {
+                Console.WriteLine("Your answer needs to be an integer. Try again.");
+                result = Console.ReadLine();
+            }
+            return result;
+        }
+
+        internal static string GetName()
+        {
+            Console.WriteLine("Please tell your name?");
+            var name = Console.ReadLine();
+
+            while (string.IsNullOrEmpty(name))
+            {
+                Console.Write("Name can't be empty.");
+                name = Console.ReadLine();  
+            }
+            return name;
+        }
+    }
+}
---
+++ FirstProgram/Menu.cs
@@ -0,0 +1,62 @@
+﻿namespace FirstProgram
+{
+    internal class Menu
+    {
+        GameEngine gameEngine = new();
+
+        internal void ShowMenu(string name, DateTime date)
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------");
+            Console.WriteLine($"Hello {name.ToUpper()}. It's {date.DayOfWeek}. This is your math's game. That's great that you're working on improving yourself");
+            Console.WriteLine("Press any key to show menu.");
+            Console.ReadLine();
+            Console.WriteLine("\n");
+
+            var isGameOn = true;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine($@"
+What game you would like to play today? Choose from the below options:
+V - View previous games
+A - Addition
+S - Subtraction
+M - Multiplication
+D - Division
+Q - Quit the program.");
+                Console.WriteLine("----------------------");
+
+                var gameSelected = Console.ReadLine();
+
+                switch (gameSelected.Trim().ToLower())
+                {
+                    case "v":
+                        Helpers.PrintGames();
+                        break;
+                    case "a":
+                        gameEngine.AdditionGame("Addition game.");
+                        break;
+                    case "s":
+                        gameEngine.SubtractionGame("Subtraction game.");
+                        break;
+                    case "m":
+                        gameEngine.MultiplicationGame("Mutiplication game.");
+                        break;
+                    case "d":
+                        gameEngine.DivisionGame("Division game.");
+                        break;
+                    case "q":
+                        Console.WriteLine("Goodbye.");
+                        isGameOn = false;
+                        Environment.Exit(1);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+            } while (isGameOn);
+        }
+    }
+}
